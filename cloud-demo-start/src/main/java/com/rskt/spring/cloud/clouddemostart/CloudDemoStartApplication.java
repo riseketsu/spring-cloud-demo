8536cc1a5bc774c55c7233c70a02f76cf8768e93
riseketsu <riseketsu@163.com>
2018-09-20 16:14:00 +0800

    add

---
+++ cloud-demo-start/src/main/java/com/rskt/spring/cloud/clouddemostart/CloudDemoStartApplication.java
@@ -0,0 +1,12 @@
+package com.rskt.spring.cloud.clouddemostart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDemoStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDemoStartApplication.class, args);
+	}
+}
---
+++ cloud-demo-start/src/test/java/com/rskt/spring/cloud/clouddemostart/CloudDemoStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rskt.spring.cloud.clouddemostart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudDemoStartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
