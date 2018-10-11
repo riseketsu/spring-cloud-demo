d34d733eaf2d00968499e8e07178e1d336a3f73c
riseketsu <riseketsu@163.com>
2018-10-11 17:27:16 +0800

    oauth2

--- cloud-demo-start/src/main/java/com/rskt/spring/cloud/security/OAuth2SecurityConfiguration.java
+++ cloud-demo-start/src/main/java/com/rskt/spring/cloud/security/OAuth2SecurityConfiguration.java
@@ -5,6 +5,7 @@ package com.rskt.spring.cloud.security;
 
 import javax.servlet.Filter;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
